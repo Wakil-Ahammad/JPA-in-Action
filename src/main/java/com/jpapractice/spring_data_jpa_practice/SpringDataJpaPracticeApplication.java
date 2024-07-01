ea5d413a5816682ea73cfb74d25e31e00a28980b
wakil <wakil.ahammad@bracits.com>
2024-07-01 18:08:11 +0600

    first commit

---
+++ src/main/java/com/jpapractice/spring_data_jpa_practice/SpringDataJpaPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.jpapractice.spring_data_jpa_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaPracticeApplication.class, args);
+	}
+
+}
