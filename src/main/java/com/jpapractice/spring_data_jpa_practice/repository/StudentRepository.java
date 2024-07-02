153f9448c29a9aa5ccc6d52d9eeb594fd950b2c9
Wakil Ahammad <69745195+Wakil545@users.noreply.github.com>
2024-07-02 10:33:23 +0600

    Home first commit

--- src/main/java/com/jpapractice/spring_data_jpa_practice/repository/StudentRepository.java
+++ src/main/java/com/jpapractice/spring_data_jpa_practice/repository/StudentRepository.java
@@ -3,6 +3,9 @@ package com.jpapractice.spring_data_jpa_practice.repository;
 import com.jpapractice.spring_data_jpa_practice.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+    public List<Student> findByFirstName(String firstName);
 }
