8cf04cb5128f2ac40d3e16d8d61e2aec6601d902
wakil <wakil.ahammad@bracits.com>
2024-07-04 19:05:07 +0600

    fourth commit from office

--- src/test/java/com/jpapractice/spring_data_jpa_practice/repository/CourseRepositoryTest.java
+++ src/test/java/com/jpapractice/spring_data_jpa_practice/repository/CourseRepositoryTest.java
@@ -5,7 +5,12 @@ import com.jpapractice.spring_data_jpa_practice.entity.Teacher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -36,4 +41,21 @@ class CourseRepositoryTest {
 
         courseRepository.save(course);
     }
+
+    @Test
+    public void findAllPagination(){
+        Pageable firstPageWithThreeRecords =
+                PageRequest.of(0,3);
+
+        Pageable secondPageWithTwoRecords =
+                 PageRequest.of(1,2);
+
+        List<Course> courses = courseRepository.findAll(firstPageWithThreeRecords).getContent();
+
+        System.out.println("courses: " + courses);
+
+    }
+
+
+
 }
